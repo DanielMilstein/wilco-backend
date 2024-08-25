ec4201b399e5c68f07512966791bd87cd5da218f
DanielMilstein <dnmilstein@miuandes.cl>
2024-08-24 21:33:42 -0400

    cambios cata

--- clips/views.py
+++ clips/views.py
@@ -5,7 +5,6 @@ from .models import Clip
 from .serializers import ClipSerializer
 from django.shortcuts import render
 import os
-from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
@@ -32,10 +31,11 @@ messages = [
     HumanMessagePromptTemplate.from_template("{user_message}"),
 ]
 
-
+# Crear la cadena de prompts
 chain = ChatPromptTemplate.from_messages(messages) | model | StrOutputParser()
-
-
+# Variables globales
+message = ""
+long_message = False
 
 
 # Create your views here.
@@ -51,12 +51,53 @@ def view_clip(request, id):
 
 @api_view(['POST'])
 def api_create_clip(request):
+    global message, long_message
     if request.method == 'POST':
         serializer = ClipSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            transcription = serializer.validated_data.get('transcription')
+            #Cuando llega un clip mandar al LLM para ver si encuentra uno de los códigos 
+            # de inicio y o término
+            if long_message == True:
+                message = message+" "+transcription
+                print(f"Mensaje enviado al LLM {message}")
+                response = classify_message(message)
+                if response == "0":
+                    print(f"LLamar {message}")
+                    message = ""
+                    long_message = False
+
+            elif long_message == False:
+                print(f"Mensaje enviado al LLM {transcription}")
+                response = classify_message(transcription)
+                if response == "0":
+                    print(f"LLamar {transcription}")
+                    message = ""
+                    long_message = False
+                elif response == "1" or response == "3":
+                    message += transcription
+                    long_message = True
+
+            
+            print(f"long_message: {long_message}")
+            print(f"message: {message}")
+        
+           
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@traceable
+def classify_message(message):
+    try:
+        print(f"El mensaje es: '{message}'")
+        response = chain.invoke({"history": history, "user_message": message})
+        return response
+    except Exception as e:
+        return f"Hubo un error al procesar tu solicitud. Por favor, intenta de nuevo. {e}"
+    
 
 @api_view(['GET'])
 def api_list_clips(request):
