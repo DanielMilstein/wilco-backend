d3bfae7449d5098ebf18720b564c27f7ccfc6351
PepeLira <jflira@miuandes.cl>
2024-08-25 05:19:10 -0400

    frontend

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.home, name='home'),
-    
-    
-
+    path('filter_clips/', views.filter_clips, name='filter_clips'),
+    path('filter_clips2/', views.filter_clips2, name='filter_clips2'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
+from clips.models import Clip
+from datetime import datetime
+
+SELECTED_DATE = 'SELECTED_DATE'
 
 # Create your views here.
-
 def home(request):
-    return render(request, 'home.html')
+    global SELECTED_DATE
+
+    current_date = datetime.now().date()
+    SELECTED_DATE = current_date
+    clips = Clip.objects.filter(date=current_date).order_by('-time_start')
+    dates = Clip.objects.values('date').distinct()
+
+    return render(request, 'main.html', {'clips': clips, 'current_date': current_date, 'dates': dates})
 
 def register(request):
     if request.method == 'POST':
@@ -21,3 +31,26 @@ def register(request):
         form = CustomUserCreationForm()
 
     return render(request, 'register.html', {'form': form})
+
+def filter_clips(request):
+    global SELECTED_DATE
+    SELECTED_DATE = request.GET.get('date')  # Get the date from the request
+    SELECTED_DATE = datetime.strptime(SELECTED_DATE, '%d/%m/%Y').date()  # Reformat the date
+    clips = Clip.objects.filter(date=SELECTED_DATE).order_by('-time_start')  # Filter clips by the selected date
+    return render(request, 'partials/clips_list.html', {'clips': clips})  # Render partial template
+
+def filter_clips2(request):
+    global SELECTED_DATE  # Use the global SELECTED_DATE
+
+    # Check if SELECTED_DATE is set and is in the correct format (YYYY-MM-DD)
+    if SELECTED_DATE:
+        try:
+            # Parse the date to ensure it is in the correct format
+            SELECTED_DATE = datetime.strptime(str(SELECTED_DATE), '%Y-%m-%d').date()
+        except ValueError:
+            # Handle the case where the date is not in the correct format
+            SELECTED_DATE = datetime.now().date()  # Default to today's date if parsing fails
+
+    # Filter clips by the selected date
+    clips = Clip.objects.filter(date=SELECTED_DATE).order_by('-time_start')
+    return render(request, 'partials/clips_list.html', {'clips': clips})
