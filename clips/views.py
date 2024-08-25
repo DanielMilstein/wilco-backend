f13d783f1e5922d617cdf37fe023fc54f3dda4c9
DanielMilstein <dnmilstein@miuandes.cl>
2024-08-25 10:02:42 -0400

    send report test 1

--- clips/views.py
+++ clips/views.py
@@ -10,7 +10,12 @@ from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langsmith import traceable
-
+import re
+import what3words
+import requests
+import json
+from django.http import JsonResponse
+from reports.views import send_report
 
 
 model = ChatOpenAI(model="gpt-4o-mini")
@@ -20,19 +25,26 @@ history = [
 
 messages = [
     SystemMessage(content=(
-        "Tu tarea es clasificar el mensaje basado en la presencia de ciertas claves. "
-        "Clasifica el mensaje de la siguiente manera: "
-        "Devuelve '0' si el mensaje contiene tanto una clave de inicio (R20, R22, A7) como la clave de fin (E1). "
-        "Devuelve '1' si el mensaje contiene solo una clave de inicio (R20, R22, A7) pero no contiene la clave de fin (E1). "
-        "Devuelve '2' si el mensaje contiene solo la clave de fin (E1) pero no contiene ninguna clave de inicio (R20, R22, A7). "
-        "Devuelve '3' si el mensaje no contiene ninguna de las claves mencionadas."
+        "Eres una central de asistencia de emergencia. Recibes una lista de dos elementos. "
+        "El primer elemento es un código de activación que indica el tipo de emergencia, "
+        "y el segundo elemento es el lugar donde ocurrió. Tu tarea es traducir el código de activación "
+        "y generar un mensaje de alerta que indique a las unidades que se dirijan al lugar indicado."
+        "\n\n"
+        "Si el código de activación es:\n"
+        "- 'A7': Genera el mensaje 'Alerta: Filtración de gases peligrosos en {lugar}. Diríjanse de inmediato.'\n"
+        "- 'R20': Genera el mensaje 'Alerta: Choque en {lugar}. Diríjanse de inmediato.'\n"
+        "- 'R22': Genera el mensaje 'Alerta: Incendio Forestal con información en {lugar}. Diríjanse de inmediato.'"
     )),
     MessagesPlaceholder(variable_name="history"),
     HumanMessagePromptTemplate.from_template("{user_message}"),
 ]
-
-# Crear la cadena de prompts
 chain = ChatPromptTemplate.from_messages(messages) | model | StrOutputParser()
+
+API_KEY = '31PTULZ0'
+W3W_API_URL = 'https://api.what3words.com/v3/'
+geocoder = what3words.Geocoder(API_KEY)
+
+
 # Variables globales
 message = ""
 long_message = False
@@ -57,46 +69,247 @@ def api_create_clip(request):
         if serializer.is_valid():
             serializer.save()
             transcription = serializer.validated_data.get('transcription')
-            #Cuando llega un clip mandar al LLM para ver si encuentra uno de los códigos 
-            # de inicio y o término
             if long_message == True:
-                message = message+" "+transcription
-                print(f"Mensaje enviado al LLM {message}")
+                message = message + " " + transcription
+                #print(f"Mensaje enviado al LLM {message}")
                 response = classify_message(message)
                 if response == "0":
-                    print(f"LLamar {message}")
+                    manejar_mensaje_completo(message)
                     message = ""
                     long_message = False
-
-            elif long_message == False:
+                elif response == "2":
+                    print(F"Clave de inicio, no fue captada. Por favor repetir mensaje")
+            else:
                 print(f"Mensaje enviado al LLM {transcription}")
                 response = classify_message(transcription)
                 if response == "0":
-                    print(f"LLamar {transcription}")
+                    message = transcription
+                    claves,direccion,mensaje_alerta = manejar_mensaje_completo(message)
+                    manejar_alerta(claves,direccion,mensaje_alerta)
                     message = ""
                     long_message = False
                 elif response == "1" or response == "3":
                     message += transcription
                     long_message = True
-
+                elif response == "2":
+                    print(F"Clave de inicio, no fue captada. Por favor repetir mensaje")
             
-            print(f"long_message: {long_message}")
-            print(f"message: {message}")
-        
-           
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+def obtener_claves(mensaje):
+    # Expresión regular ajustada para manejar casos como R20, R 20, y R,20
+    #print(f"mensaje {mensaje}")
+    pattern = r"\b(R\s*,?\s*20|R\s*,?\s*22|A\s*,?\s*7|E\s*,?\s*1)\b"
+    
+    # Buscar todas las coincidencias en el string
+    matches = re.findall(pattern, mensaje)
+    
+    # Limpiar las claves encontradas (eliminar espacios y comas)
+    claves = [re.sub(r"[\s,]", "", match) for match in matches]
+    
+    #print(f"Claves: {claves}")
+    return claves
 
-@traceable
-def classify_message(message):
+def separar_claves_y_texto(mensaje, claves):
+    # Expresión regular flexible para las claves ya obtenidas
+    pattern = r"|".join([re.escape(clave) for clave in claves])
+    pattern = fr"\b({pattern})\b"
+
+    # Lista para almacenar las partes separadas
+    partes_separadas = []
+    ultimo_indice = 0
+
+    # Recorrer las coincidencias de la expresión regular
+    for match in re.finditer(pattern, mensaje):
+        inicio, fin = match.span()
+        # Agregar el texto intermedio a la lista, si existe
+        if inicio > ultimo_indice:
+            texto_intermedio = mensaje[ultimo_indice:inicio].strip(", ")
+            if texto_intermedio:
+                partes_separadas.append(texto_intermedio)
+        # Agregar la clave encontrada a la lista
+        partes_separadas.append(match.group())
+        ultimo_indice = fin
+
+    # Agregar cualquier texto restante después de la última clave
+    if ultimo_indice < len(mensaje):
+        texto_restante = mensaje[ultimo_indice:].strip(", ")
+        if texto_restante:
+            partes_separadas.append(texto_restante)
+
+    return partes_separadas
+
+def procesar_mensaje(mensaje):
+    claves = obtener_claves(mensaje)
+    #print(f"Claves encontradas: {claves}")
+    partes_separadas = separar_claves_y_texto(mensaje, claves)
+    
+    return claves,partes_separadas
+
+def ordenar_mensaje(partes_separadas):
+    #print(f"Entrando partes separadas {partes_separadas}")
+    mensaje_ordenado = []
+    for parte in partes_separadas:
+        if parte in ["R20","R22","A7"]:
+            mensaje_ordenado.insert(0,parte)
+        elif len(mensaje_ordenado)>0 and parte not in ["E,1","E1"]:
+            mensaje_ordenado.append(parte)
+        elif parte in ["E,1","E1"]:
+            break
+    mensaje_ordenado.append("E1")
+    #print(f"Mensaje ordenado: {mensaje_ordenado} ")
+
+    return mensaje_ordenado
+
+def separar_string(texto):
+    partes = re.split(r'[,\s]+', texto)
+    partes = [parte for parte in partes if parte]
+    return partes
+
+def encontrar_clave_coordenadas(mensaje_ordenado):
+    claves_coordenadas = []
+    if len(mensaje_ordenado)>1:
+        clave_coordenadas = mensaje_ordenado[1]
+        claves = separar_string(clave_coordenadas)
+        for i in range(3):
+            try:
+                claves_coordenadas.append(claves[i])
+            except:
+                pass
+
+    return claves_coordenadas
+
+def traducir_coordenadas(clave_coordenadas):
+    direccion = ""
+    for lugar in clave_coordenadas:
+        direccion = direccion +"." + lugar
+    
+    direccion = direccion.strip('.')
+    direccion= re.sub(r'\.+', '.', direccion)
+    direccion_traducida = get_address(direccion)
+    print(f"direccion {direccion}")
+    print(f"direccion {direccion_traducida}")
+    return direccion_traducida
+
+def generar_alerta(clave_coordenadas,direccion):
     try:
-        print(f"El mensaje es: '{message}'")
-        response = chain.invoke({"history": history, "user_message": message})
+        codigo_activacion = clave_coordenadas[0]
+        user_message = f"El código de activación es {codigo_activacion} y el lugar es {direccion}."
+        response = chain.invoke({"history": history, "user_message": user_message})
+        print(f"Mensaje de alerta: {response}")
         return response
     except Exception as e:
-        return f"Hubo un error al procesar tu solicitud. Por favor, intenta de nuevo. {e}"
+        print(f"Error al invocar el modelo: {e}")
+        return "Hubo un error al procesar tu solicitud. Por favor, intenta de nuevo."
+
+
+def manejar_alerta(claves, direccion, mensaje_alerta):
+    all_phone_numbers = {
+        'bomberos'    :  "+56990972126",
+        'ambulancia'  :  "+56942545982",
+        'carabineros' : "+56968404532"
+                    }
+    try:
+        clave_inicio = claves[0]
+        if clave_inicio in ["R20","R,20","r20"]:
+            print("CHOQUE, llamar a bomberos y ambulancia")
+            print(mensaje_alerta)
+            # Llamar a send report con el mensaje generado con json y los números de teléfono
+            title = "Emergencia_R20"
+            summary = mensaje_alerta
+            phone_numbers = [all_phone_numbers['bomberos'], all_phone_numbers['ambulancia']]
+            send_report(title = title, summary = summary, phone_numbers = phone_numbers)
+
+        elif clave_inicio in ["R22","R,22","r22"]:
+            print("INCENDIO FORESTAL BIDUBIDU LLAMAR A BOMBEROS y carabineros")
+            print(mensaje_alerta)
+            # Llamar a send report con el mensaje generado con json y los números de teléfono
+            title = "Emergencia_R22"
+            summary = mensaje_alerta
+            phone_numbers = [all_phone_numbers['bomberos'], all_phone_numbers['carabineros']]
+            send_report(title = title, summary = summary, phone_numbers = phone_numbers)
+
+        elif clave_inicio in ["A7","A,7","a7"]:
+            print("QUIMICOS llamar a bomberos y ambulancia")
+            print(mensaje_alerta)
+            # Llamar a send report con el mensaje generado con json y los números de teléfono
+            title = "Emergencia_A7"
+            summary = mensaje_alerta
+            phone_numbers = [all_phone_numbers['bomberos'], all_phone_numbers['ambulancia']]
+            send_report(title = title, summary = summary, phone_numbers = phone_numbers)
+
+    except Exception as e:
+        print(f"Error al manejar la alerta: {e}")
+
+
+
+
+def classify_message(message):
+    # Define the regular expression patterns for start and end keys
+    start_keys_pattern = r"\b(R\s*,?\s*20|R\s*,?\s*22|A\s*,?\s*7)\b"
+    end_key_pattern = r"\b(E\s*,?\s*1)\b"
+    
+    # Find start and end keys in the message
+    start_keys = re.findall(start_keys_pattern, message)
+    end_keys = re.findall(end_key_pattern, message)
+    
+    # Classify the message based on the presence of start and end keys
+    if start_keys and end_keys:
+        return "0"  # Both start and end keys are present
+    elif start_keys and not end_keys:
+        return "1"  # Only start keys are present
+    elif not start_keys and end_keys:
+        return "2"  # Only end key is present
+    else:
+        return "3"  # Neither start nor end keys are present
+
+def manejar_mensaje_completo(message):
+    #print(f"LLamar {message}")
+    claves, partes_separadas = procesar_mensaje(message)
+    #print(f"Claves y texto: {claves}")
+    
+    mensaje_ordenado = ordenar_mensaje(partes_separadas)
+    clave_coordenadas = encontrar_clave_coordenadas(mensaje_ordenado)
+    
+    #print(f"COORDENADAS: {clave_coordenadas}")
+    
+    direccion = traducir_coordenadas(clave_coordenadas)
+    #ENVIAR A PEPE
+    
+    generar_alerta(claves, clave_coordenadas)
+
+    # Llamar a send report con el mensaje generado
+
+
+
+
+
+
+def get_address(direccion_traducida):
+    api_key = API_KEY
+    words = direccion_traducida
+    try:
+        url = 'https://api.what3words.com/v3/convert-to-coordinates?'
+        params = {
+            'words': words,
+            'key': api_key,
+            'lang': 'es'
+            }
+        response = requests.get(url, params=params)
+        response_data = response.json()
+        print(response_data, "URL")
+        print(api_key)
+
+        if 'coordinates' in response_data:
+            lat = response_data['coordinates']['lat']
+            lon = response_data['coordinates']['lng']
+            return (f"latitude {lat} longitude {lon}")
+    except:
+        return (f"No se encontraron las coordenadas")
+      
+
     
 
 @api_view(['GET'])
--- reports/views.py
+++ reports/views.py
@@ -220,3 +220,41 @@ def api_send_report(request):
 
 
         return Response(status=status.HTTP_200_OK)
+
+
+
+
+
+def send_report(title, summary, phone_numbers):
+    if request.method == 'POST':
+        # disclaimer = 'The TTS voice you are hearing is AI-generated and not a human voice.'
+        disclaimer = 'La voz TTS que está escuchando es generada por IA y no es una voz humana.'
+
+        response = client.audio.speech.create(
+            model = 'tts-1-hd',
+            voice = 'onyx',
+            input = disclaimer + summary
+        )
+
+        file_name = f'{title}.mp3'
+        bucket_name = 'tts.clips'
+        file_url = f"https://s3.amazonaws.com/{bucket_name}/{file_name}"
+
+
+        response.stream_to_file(file_name)
+
+        try:
+            s3.upload_file(file_name, bucket_name, file_name, ExtraArgs={'GrantRead': 'uri="http://acs.amazonaws.com/groups/global/AllUsers"', 'ContentType': 'audio/mp3'})
+            print(f'{file_url} uploaded to S3')
+        except NoCredentialsError:
+            print("Credentials not available")
+
+
+        for phone_number in phone_numbers:
+            print(f'Sending report to {phone_number}')
+            call = twilio_client.calls.create(
+                twiml=f'<Response><Play loop="2">{file_url}</Play></Response>',
+                to=phone_number,
+                from_=os.environ["MY_TWILIO_NUMBER"]
+            )
+            
